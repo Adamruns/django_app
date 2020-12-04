d3865a5e5d96c2880320a6861132d53f000dd8f4
Adam Smith <adam.smith@greermiddlecollege.org>
2020-12-04 10:13:17 -0500

    Trying to fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# Activate Django-Heroku
+# Activate Django-Heropip freeze
 django_heroku.settings(locals())
